9f8c0596c2c0b018a2ac8e72a603caead460d1f0
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-11-20 21:48:03 +0200

    commit #7, update

--- src/main/java/com/MyQuizAppSocialSecurity/rest/AdminController.java
+++ src/main/java/com/MyQuizAppSocialSecurity/rest/AdminController.java
@@ -1,5 +1,7 @@
 package com.MyQuizAppSocialSecurity.rest;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,107 +28,144 @@ import com.MyQuizAppSocialSecurity.beans.SuggestedQuestion;
 @PropertySource(value = "classpath:info.properties")
 @Lazy
 public class AdminController {
-	//need to check what PUT request return ASAP!!!!!!!!
-	//need to check what PUT request return ASAP!!!!!!!!
-	//need to check what PUT request return ASAP!!!!!!!!
-	//need to check what PUT request return ASAP!!!!!!!!
-	//need to check what PUT request return ASAP!!!!!!!!
-	//need to check what PUT request return ASAP!!!!!!!!
-
+	// need to check what PUT request return ASAP!!!!!!!!
+	// need to check what PUT request return ASAP!!!!!!!!
+	// need to check what PUT request return ASAP!!!!!!!!
+	// need to check what PUT request return ASAP!!!!!!!!
+	// need to check what PUT request return ASAP!!!!!!!!
+	// need to check what PUT request return ASAP!!!!!!!!
 
 	private String BASE_QUIZ_ADMIN_URL;
 	private ResponseEntity<?> responseEntity;
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@PostConstruct
 	private void setProperties() {
 		BASE_QUIZ_ADMIN_URL = env.getProperty("baseQuizAdminURL");
 	}
-	
-	
+
 	@PostMapping("/addQuestion")
 	public ResponseEntity<?> addQuestion(@RequestBody Question question) {
-		if(question != null) {
+		restartValues();
+		if (question != null) {
 			responseEntity = restTemplate.postForEntity(BASE_QUIZ_ADMIN_URL + "/addQuestion", question, String.class);
-			if(responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
+			if (responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
 				return ResponseEntity.status(HttpStatus.OK).body(responseEntity.getBody());
-			}else {
+			} else {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseEntity.getBody());
 			}
-		}else {
+		} else {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input");
 		}
 	}
-	
+
 	@PutMapping("/updateQuestion")
 	public ResponseEntity<?> updateQuestion(@RequestBody Question question) {
-		if(question != null) {
+		restartValues();
+		if (question != null) {
 			try {
 				restTemplate.put(BASE_QUIZ_ADMIN_URL + "/updateQuestion", question);
 				return ResponseEntity.status(HttpStatus.OK).body("Question updated");
 			} catch (Exception e) {
-				//need to check what PUT request return ASAP!!!!!!!!
+				// need to check what PUT request return ASAP!!!!!!!!
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 			}
-		}else {
+		} else {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input");
 		}
 	}
-	
+
 	@DeleteMapping("/removeQuestion/{questionId}")
 	public ResponseEntity<?> removeQuestion(@PathVariable long questionId) {
-		if(questionId >= 0) {
+		restartValues();
+		if (questionId >= 0) {
 			try {
 				restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/updateQuestion" + "/" + questionId);
 				return ResponseEntity.status(HttpStatus.OK).body("Question removed");
 			} catch (Exception e) {
-				//need to check what PUT request return ASAP!!!!!!!!
+				// need to check what DELETE request return ASAP!!!!!!!!
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 			}
-		}else {
+		} else {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input");
 		}
 	}
-	
+
 	@GetMapping("/getSuggestedQuestions")
-	public ResponseEntity<?> getAllSuggestedQuestions(){
-		
-		return restTemplate.getForEntity(BASE_QUIZ_ADMIN_URL + "/getSuggestedQuestions", ResponseEntity.class);
+	public ResponseEntity<?> getAllSuggestedQuestions() {
+		restartValues();
+		responseEntity = restTemplate.getForEntity(BASE_QUIZ_ADMIN_URL + "/getSuggestedQuestions", List.class);
+		if (responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
+			return ResponseEntity.status(HttpStatus.OK).body(responseEntity.getBody());
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There are no suggested questions");
+		}
 	}
-	
+
 	@PostMapping("/addSuggestedQuestion")
-	public ResponseEntity<?> addSuggestedQuestion(SuggestedQuestion suggestedQuestion){
-		return restTemplate.postForEntity(BASE_QUIZ_ADMIN_URL + "/addSuggestedQuestion", suggestedQuestion, ResponseEntity.class);
+	public ResponseEntity<?> addSuggestedQuestion(SuggestedQuestion suggestedQuestion) {
+		restartValues();
+		if (suggestedQuestion != null) {
+			responseEntity = restTemplate.postForEntity(BASE_QUIZ_ADMIN_URL + "/addSuggestedQuestion",
+					suggestedQuestion, String.class);
+			if (responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
+				return ResponseEntity.status(HttpStatus.OK).body(responseEntity.getBody());
+			} else {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseEntity.getBody());
+			}
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input");
+		}
 	}
-	
+
 	@PostMapping("/addAllSuggestedQuestions")
-	public ResponseEntity<?> addAllSuggestedQuestions(){
-		return restTemplate.postForEntity(BASE_QUIZ_ADMIN_URL + "/addAllSuggestedQuestions", null, ResponseEntity.class);
+	public ResponseEntity<?> addAllSuggestedQuestions() {
+		restartValues();
+		responseEntity = restTemplate.postForEntity(BASE_QUIZ_ADMIN_URL + "/addAllSuggestedQuestions", null,
+				ResponseEntity.class);
+		if (responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
+			return ResponseEntity.status(HttpStatus.OK).body(responseEntity.getBody());
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseEntity.getBody());
+		}
 	}
-	
-	//need to check how i can return a response here!!!
+
+	// need to check how i can return a response here!!!
 	@DeleteMapping("/deleteSuggestedQuestion/{sqID}")
-	public ResponseEntity<?> deleteSuggestedQuestion(@PathVariable("sqID") long suggestedQuestionID){
-		restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/deleteSuggestedQuestion" + "/" + suggestedQuestionID);
-		return null;
+	public ResponseEntity<?> deleteSuggestedQuestion(@PathVariable("sqID") long suggestedQuestionID) {
+		restartValues();
+		if (suggestedQuestionID > 0) {
+			try {
+				restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/deleteSuggestedQuestion" + "/" + suggestedQuestionID);
+				return ResponseEntity.status(HttpStatus.OK).body("Suggested questio deleted");
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+			}
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input");
+		}
 	}
-	
-	//need to check how i can return a response here!!!
+
+	// need to check how i can return a response here!!!
 	@DeleteMapping("/deleteAllSuggestedQuestions")
-	public ResponseEntity<?> deleteAllSuggestedQuestions(){
-		restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/deleteAllSuggestedQuestions");
-		return null;
+	public ResponseEntity<?> deleteAllSuggestedQuestions() {
+		restartValues();
+		try {
+			restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/deleteAllSuggestedQuestions");
+			return ResponseEntity.status(HttpStatus.OK).body("All suggested questions deleted");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}		
 	}
-	
-	//need to check how i can return a response here!!!
-	@DeleteMapping("/deleteExpiredQuizs")
-	public void deleteExpiredQuizs() {
-		restTemplate.delete(BASE_QUIZ_ADMIN_URL + "/deleteExpiredQuizs");
+
+	private void restartValues() {
+		responseEntity = null;
+		restTemplate = null;
 	}
-	
+
 }
